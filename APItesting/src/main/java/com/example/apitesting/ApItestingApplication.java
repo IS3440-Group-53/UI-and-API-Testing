0ad213cdcfe8b995074a66f5788675e1dba2940e
dasuni_wijethilaka <dasunichethanadc@gmail.com>
2024-12-16 19:32:45 +0530

    Initial commit

---
+++ APItesting/src/main/java/com/example/apitesting/ApItestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApItestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApItestingApplication.class, args);
+    }
+
+}
---
+++ UItesting/src/test/java/com/example/uitesting/UItestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uitesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UItestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
