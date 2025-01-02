ad035515a535c2deda333716fef31b6ec2922954
PrabathAnuradhaDharmasiri <dharmasiripa.20@uom.lk>
2025-01-02 10:10:13 +0530

    Time tab UI test cases Time sheets and attendence

--- src/main/java/Pages/TimePage.java
+++ src/main/java/Pages/TimePage.java
@@ -1,58 +1,3 @@
-//package Pages;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import java.time.Duration;
-//
-//public class TimePage {
-//    WebDriver driver;
-//    WebDriverWait wait;
-//
-//    By timeTab = By.xpath("/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[4]/a");
-//    By timesheetsDropdown = By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span");
-//    By myTimesheets = By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li[1]");
-//    By pageHeader = By.xpath("//h6[contains(text(),'Time / Timesheets')]");
-//    By noRecordsFound = By.xpath("//td[text()='No Records Found']");
-//
-//    public TimePage(WebDriver driver) {
-//        this.driver = driver;
-//        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-//    }
-//
-//    public void clickTimeTab() {
-//        try {
-//            Thread.sleep(2000);
-//            wait.until(ExpectedConditions.elementToBeClickable(timeTab)).click();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public void clickTimesheetsTab() {
-//        try {
-//            Thread.sleep(2000);
-//            wait.until(ExpectedConditions.elementToBeClickable(timesheetsDropdown)).click();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public void clickMyTimesheets() {
-//        wait.until(ExpectedConditions.elementToBeClickable(myTimesheets)).click();
-//    }
-//
-//    public void verifyTimePageHeader() {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(pageHeader));
-//    }
-//
-//    public void verifyTimesheetTable() {
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(noRecordsFound));
-//    }
-//}
-
-//(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((***********************************
 
 package Pages;
 
@@ -66,6 +11,7 @@ public class TimePage {
     WebDriver driver;
     WebDriverWait wait;
 
+    // Previous locators remain the same
     By timeTab = By.xpath("/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[4]/a");
     By timesheetsDropdown = By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span");
     By myTimesheets = By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li[1]");
@@ -73,12 +19,19 @@ public class TimePage {
     By editButton = By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/form/div[3]/div[2]/button[1]");
     By typeField = By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/form/div[2]/table/tbody/tr[1]/td[1]/div/div[2]/div/div/input");
     By saveButton = By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/form/div[3]/div[2]/button[3]");
+    By attendanceDropdown = By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[2]/span");
+    By punchInOutButton = By.xpath("/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul/li[2]/a");
+    By noteTextField = By.xpath("//textarea[@placeholder='Type here']");
+
+    // Updated XPath for the In/Out button
+    By inOutButton = By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/div/form/div[3]/button");
 
     public TimePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
+    // Previous methods remain the same
     public void clickTimeTab() {
         try {
             Thread.sleep(2000);
@@ -111,5 +64,42 @@ public class TimePage {
 
     public void clickSave() {
         wait.until(ExpectedConditions.elementToBeClickable(saveButton)).click();
+    }
+
+    public void clickAttendanceDropdown() {
+        try {
+            Thread.sleep(2000);
+            wait.until(ExpectedConditions.elementToBeClickable(attendanceDropdown)).click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clickPunchInOut() {
+        try {
+            Thread.sleep(2000);
+            wait.until(ExpectedConditions.elementToBeClickable(punchInOutButton)).click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void enterNoteText() {
+        try {
+            Thread.sleep(2000);
+            wait.until(ExpectedConditions.elementToBeClickable(noteTextField)).sendKeys("Test");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Updated method for clicking In/Out button
+    public void clickInOutButton() {
+        try {
+            Thread.sleep(2000);
+            wait.until(ExpectedConditions.elementToBeClickable(inOutButton)).click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/StepDefinition/TimeStepDefinition.java
+++ src/test/java/StepDefinition/TimeStepDefinition.java
@@ -1,35 +1,3 @@
-//package StepDefinition;
-//
-//import Pages.TimePage;
-//import Utilities.DriverManager;
-//import io.cucumber.java.en.Then;
-//import io.cucumber.java.en.When;
-//
-//public class TimeStepDefinition extends DriverManager {
-//    TimePage timePage;
-//
-//    @When("I navigate to Time tab")
-//    public void navigateToTimeTab() {
-//        timePage = new TimePage(driver);
-//        timePage.clickTimeTab();
-//    }
-//
-//    @When("I click on Timesheets tab")
-//    public void clickTimesheetsTab() {
-//        timePage.clickTimesheetsTab();
-//    }
-//
-//    @When("I select My Timesheets")
-//    public void selectMyTimesheets() {
-//        timePage.clickMyTimesheets();
-//    }
-//
-//    @Then("I should see the Time page")
-//    public void verifyTimePage() {
-//        timePage.verifyTimePageHeader();
-//        timePage.verifyTimesheetTable();
-//    }
-//}
 
 package StepDefinition;
 
@@ -63,5 +31,13 @@ public class TimeStepDefinition {
         timePage.clickEdit();
         timePage.enterTestInTypeField();
         timePage.clickSave();
+    }
+
+    @When("I proceed to Attendance")
+    public void proceedToAttendance() {
+        timePage.clickAttendanceDropdown();
+        timePage.clickPunchInOut();
+        timePage.enterNoteText();
+        timePage.clickInOutButton();  // Updated to use new method
     }
 }
