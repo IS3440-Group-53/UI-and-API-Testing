142900f60904f4ca6bb9b0525fc649f2d8c28f61
PrabathAnuradhaDharmasiri <dharmasiripa.20@uom.lk>
2025-01-04 17:01:35 +0530

    pipeline setup

--- src/main/java/Utilities/DriverManager.java
+++ src/main/java/Utilities/DriverManager.java
@@ -3,6 +3,7 @@ package Utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
 
 public class DriverManager {
@@ -12,6 +13,16 @@ public class DriverManager {
     @BeforeSuite
     public static WebDriver setUpSuite() {
         if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--disable-software-rasterizer");
+
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
             driver.manage().window().maximize();
@@ -23,6 +34,8 @@ public class DriverManager {
     }
     public static WebDriver getDriver() {
         if (driver == null) {
+
+
             throw new IllegalStateException("Driver has not been initialized. Call setUpSuite first.");
         }
         return driver;
